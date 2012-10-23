4d148ba67c7733923a40917a98316cf699bcf93b
Breyten Ernsting <bje@dds.nl>
2012-10-23 15:26:55 +0200

    Impelemnt user configurable settings.

---
+++ parlis_settings.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from parlis_settings_local import *
+
+settings = {
+    'parlis_api_user': PARLIS_API_USER,
+    'parlis_api_password': PARLIS_API_PASSWORD,
+}
--- parlis_utils.py
+++ parlis_utils.py
@@ -4,7 +4,10 @@ import sys
 import os
 import httplib2
 
+from parlis_settings import settings
+
 def get_http_client():
     h = httplib2.Http(disable_ssl_certificate_validation=True)
-    h.add_credentials( 'SOS', 'Open2012' )
+    #h.add_credentials( 'SOS', 'Open2012' )
+    h.add_credentials(settings['parlis_api_user'], settings['parlis_api_password'])
     return h
