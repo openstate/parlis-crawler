dcaeb9114a3003735f7257a927cb4c77c88fb4e1
Breyten Ernsting <bje@dds.nl>
2012-12-03 14:58:37 +0100

    Tabs and spaces ...

--- zaken_subtree.py
+++ zaken_subtree.py
@@ -15,32 +15,32 @@ h = get_http_client()
 logger = logging.getLogger(__name__)
 
 def parse_atom(path):
-	for subtree in ['ZaakActoren', 'Statussen', 'KamerstukDossier', 'Documenten', 'Activiteiten', 'Besluiten', 'GerelateerdVanuit', 'GerelateerdNaar', 'HoofdOverig', 'GerelateerdOverig', 'VervangenVanuit', 'VervangenDoor', 'Agendapunten']:
-		try:
-			os.mkdir(path+'_'+subtree)
-		except:
-			pass
+    for subtree in ['ZaakActoren', 'Statussen', 'KamerstukDossier', 'Documenten', 'Activiteiten', 'Besluiten', 'GerelateerdVanuit', 'GerelateerdNaar', 'HoofdOverig', 'GerelateerdOverig', 'VervangenVanuit', 'VervangenDoor', 'Agendapunten']:
+        try:
+            os.mkdir(path+'_'+subtree)
+        except:
+            pass
 
-	for filename in sorted(os.listdir(path)):
-		logger.info(filename)
-		tree = etree.parse(path + '/' + filename)
-		for elem in tree.iterfind('.//{http://www.w3.org/2005/Atom}entry/{http://www.w3.org/2005/Atom}id'):
-			logger.info(elem.text)
-			id = elem.text.split("'")[1]
-			for subtree in ['ZaakActoren', 'Statussen', 'KamerstukDossier', 'Documenten', 'Activiteiten', 'Besluiten', 'GerelateerdVanuit', 'GerelateerdNaar', 'HoofdOverig', 'GerelateerdOverig', 'VervangenVanuit', 'VervangenDoor', 'Agendapunten']:
-				subtree_file_name = path+'_'+subtree+'/%s.atom.xml' % (id,)
-				if not os.path.exists(subtree_file_name):
-    				url = elem.text + '/' + subtree
-    				resp, content = h.request( url, 'GET' )
-    				f = open(subtree_file_name, 'w')
-    				f.write(content)
-    				f.close()
+    for filename in sorted(os.listdir(path)):
+        logger.info(filename)
+        tree = etree.parse(path + '/' + filename)
+        for elem in tree.iterfind('.//{http://www.w3.org/2005/Atom}entry/{http://www.w3.org/2005/Atom}id'):
+            logger.info(elem.text)
+            id = elem.text.split("'")[1]
+            for subtree in ['ZaakActoren', 'Statussen', 'KamerstukDossier', 'Documenten', 'Activiteiten', 'Besluiten', 'GerelateerdVanuit', 'GerelateerdNaar', 'HoofdOverig', 'GerelateerdOverig', 'VervangenVanuit', 'VervangenDoor', 'Agendapunten']:
+                subtree_file_name = path+'_'+subtree+'/%s.atom.xml' % (id,)
+                if not os.path.exists(subtree_file_name):
+                    url = elem.text + '/' + subtree
+                    resp, content = h.request( url, 'GET' )
+                    f = open(subtree_file_name, 'w')
+                    f.write(content)
+                    f.close()
 
 
 #try:
-#	os.mkdir('DutchRegents/Subtree/Zaken')
+#   os.mkdir('DutchRegents/Subtree/Zaken')
 #except:
-#	pass
+#   pass
 
 if __name__ == '__main__':
     from_date = sys.argv[1]
